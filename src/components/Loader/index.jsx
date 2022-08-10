104fa692ea211c4183a20f5689e98ae026bb11a1
Jhorman Orozco <jsojhor@gmail.com>
2022-08-10 14:24:09 -0500

    change in loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,7 @@
 import './styles.css';
 export const Loader = () => {
   return (
-    <div class="lds-facebook">
+    <div className="lds-facebook">
       <div></div>
       <div></div>
       <div></div>
